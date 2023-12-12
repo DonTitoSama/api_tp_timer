869292309eb5bfb2f623e0fffb5da7f9efd5aee9
DontitoSama <jokonito06@gmail.com>
2023-12-12 14:50:32 +0100

    first commit initial project in node js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://0.0.0.0:27017/tp_1');
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const userRoute = require(`./routes/userRoute`);
+app.use('/user', userRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
